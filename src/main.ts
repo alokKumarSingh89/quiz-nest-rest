07ae4f6eed467e61f091a7886583b795546facba
Alok Singh <alok.singh@seeq.com>
2022-09-16 01:14:00 +0530

    Swagger api config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Quiz API')
+    .setDescription('Quiz , Quetion and answer API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
